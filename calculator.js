2a8f11bf0af5720482b40a7e920a3f6f30ad5925
kyletns <kyletns@gmail.com>
2015-01-23 17:22:04 -0800

    ready for students

---
+++ calculator.js
@@ -0,0 +1,27 @@
+
+function calculate(command) {
+  // Your code in here!
+}
+
+function gatherInputAndCalculate() {
+  var cmd, result;
+
+  // Grab the user input
+  cmd = $("input.cmd-input").val();
+
+  // Call the calculate function, store it's return into the result variable
+  result = calculate(cmd);
+
+  // Display the caption ("Your function returned:")
+  $("p.caption").show();
+
+  // Put the contents of the result variable into the paragraph with class 'output'
+  $("p.output").html(result);
+}
+
+$(document).ready(function() {
+  // Attach a click handler to the anchor with class 'execute'
+  // aka. When the user clicks it, call this function.
+  $("a.execute").click(gatherInputAndCalculate);
+});
+
